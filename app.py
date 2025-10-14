f23cbc1c3f50f62d8b6b0ebcebd5a229aed59e72
tomasitouch <148548556+tomasitouch@users.noreply.github.com>
2025-10-14 19:39:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # para permitir conexión desde tu tienda Shopify
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
@@ -13,4 +14,5 @@ def calcular():
     return jsonify({"resultado": potencia})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
